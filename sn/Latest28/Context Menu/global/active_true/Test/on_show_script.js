92a5d34c6ba8050a871e918b8051c5ab4f6efaca
System Administrator <admin@example.com>
2019-05-28 08:09:37 +0000

    Changes by 'System Administrator' on 'Default' -- Build #1

---
+++ sn/Latest28/Context Menu/global/active_true/Test/on_show_script.js
@@ -0,0 +1,22 @@
+/* 
+ * Application : Latest28
+ * ClassName   : sys_ui_context_menu
+ * Created On  : 2019-05-28 08:07:40
+ * Created By  : admin
+ * Updated On  : 2019-05-28 08:07:40
+ * Updated By  : admin
+ * URL         : /sys_ui_context_menu.do?sys_id=14d6aa374fa13300a3cc121f9310c7a2
+ */
+/**
+ * This client-side script is used to dynamically change the context menu before it is displayed.
+ * It is called before showing the context menu if the Run onShow script flag is checked.
+ *
+ * The following variables are available to the script:
+ *    'g_menu' the context menu that is about to be shown
+ *    'g_item' the current context menu item
+ *    'g_list' the GlideList2 that the script is running against (only valid for List context menus)
+ *    'g_fieldName' the name of the field that the context menu is running against (only valid for List context menus)
+ *    'g_fieldLabel' the label of the field that the context menu is running against (only valid for List context menus)
+ *    'g_sysId' the sys_id of the row or form that the script is running against
+ */
+ 
